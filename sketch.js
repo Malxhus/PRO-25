ffa8e5147d45fa8b349b2ac26334bb501d316a68
Malxhus <71833464+Malxhus@users.noreply.github.com>
2020-10-29 21:00:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+var boxside1, boxside2, boxbottom
+var ground
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var engine, world;
+
+function preload(){
+	
+}
+
+function setup() {
+  engine =Engine.create()
+  world = engine.world
+	createCanvas(800, 800);
+	ground = new Ground(400,620,width,20)
+  paperObject = new paper(100,90,50)
+  dustbin = new Dustbin(640,510)
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  Engine.update(engine)
+  background(255);
+  ground.display();
+  paperObject.display();
+  dustbin.display();
+}
+
+function keyPressed(){
+    if (keyCode == UP_ARROW) {
+        Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:365,y:-440});
+	}
+}
+
+
+
